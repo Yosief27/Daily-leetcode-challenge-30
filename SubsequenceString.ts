4eaed9cdfb342f05aa6292dc6f23e50203f5686d
yosief27 <josiyoniug@gmail.com>
2023-10-03 08:48:01 +0200

    Day one done!

---
+++ SubsequenceString.ts
@@ -0,0 +1,10 @@
+function isSubsequence(s:string,t:string):boolean{
+    const sourceArr=s.split("");
+    const tripArr=t.split("")
+    for(let i=0;i<sourceArr.length;i++){
+        if(tripArr.length >0 && sourceArr[i]===tripArr[0]){tripArr.shift()}
+    }
+    if(tripArr.length===0)return true
+ return false
+}
+console.log(isSubsequence("ahbgdc","agce"))
