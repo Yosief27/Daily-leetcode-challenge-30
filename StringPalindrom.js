5fae506c420b59bffebafa0d7f5d27505b9a5876
Yosief Gebremariam <56268840+Yosief27@users.noreply.github.com>
2023-10-06 02:13:09 +0000

    Delet files!

--- StringPalindrom.js
+++
@@ -1,9 +0,0 @@
-var stringPalindrom = function (s) {
-    var arrS = s.split("");
-    var palString = arrS.reduce(function (pString, ele) {
-        pString = ele.concat(pString);
-        return pString;
-    });
-    return palString !== s ? false : true;
-};
-console.log(stringPalindrom("carrac"));
